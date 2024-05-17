0559665b2768c93827bdccec386d4ec6a72d7355
Jafir Anjum <jafiranjum2020@gmail.com>
2024-05-17 22:20:29 +0600

    done student creating feature

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+// import { Schema, model, connect } from 'mongoose';
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fathercontactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  mothercontactNo: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emargencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  parmanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
